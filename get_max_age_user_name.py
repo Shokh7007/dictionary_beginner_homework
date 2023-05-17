7b1054470383a4e29ac2c5c02230673ebe821b1b
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-05-17 12:06:02 +0500

    8

--- get_max_age_user_name.py
+++ get_max_age_user_name.py
@@ -8,17 +8,30 @@ def get_max_age_user_name(data:list) -> str:
         str: The name of the user with the maximum age in the dictionary
     """
     x=0
-    for i in data:
-        if i['age']>x:
-            x=i['age']
-    return x
+    k=0
+    a=''
+    for i,j in data:
+        
+        if data[k][j]>x:
+            x=data[k][j]
+            a=data[k][i]
+        k+=1
+    return a
 print(get_max_age_user_name([
   {
     'name': 'John', 
-    'age': 27
+    'age': 32
   }, 
   {
     'name': 'Mary', 
-    'age': 42
+    'age': 48
+  }, 
+  {
+    'name': 'Ann', 
+    'age': 2878
+  }, 
+  {
+    'name': 'Ban', 
+    'age': 29
   }
 ])) 
