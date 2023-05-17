4e9558ff178bd8b21c3ca7bd3878498e701558d4
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-05-17 16:15:55 +0500

    9

--- get_user_country.py
+++ get_user_country.py
@@ -8,4 +8,36 @@ def get_user_country(data:list, name:str) -> list:
     Returns:
         str: The country of the user with the given name
     """
-    return 
+    x=0
+    k=0
+    for i,j in data:
+        if data[k][i]==name:
+            x=data[k][j]
+        k+=1
+    return x
+print(get_user_country([
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Henry', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Sam', 
+    'country': 'MEX'
+  },
+  {
+    'name': 'Kevin', 
+    'country': 'RUS'
+  },
+  {
+    'name': 'Dustin', 
+    'country': 'GER'
+  }
+],'John')) 
