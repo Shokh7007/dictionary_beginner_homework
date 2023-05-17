600af9ad30a225b44711a2246e79b694a20cf09f
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-05-17 17:09:00 +0500

    12

--- get_user_names.py
+++ get_user_names.py
@@ -8,4 +8,36 @@ def get_user_names(data:list, country:str) -> list:
     Returns:
         list: A list of users with the given country
     """
-    return 
+    x=[]
+    k=0
+    for i,j in data:
+        if data[k][j]==country:
+            x.append(data[k][i])
+        k+=1
+    return x
+print(get_user_names([
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Henry', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Sam', 
+    'country': 'MEX'
+  },
+  {
+    'name': 'Kevin', 
+    'country': 'RUS'
+  },
+  {
+    'name': 'Dustin', 
+    'country': 'GER'
+  }
+],'GER'))
